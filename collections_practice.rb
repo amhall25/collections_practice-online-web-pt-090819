914f08eb51d27059b5ae431e259a7e5564646fa0
alycia hall <amhall25@gmail.com>
2019-09-22 00:35:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array, index, destination_index)
+  array[index, destination_index] = array[destination_index, index]
+end
